b8270d4e12c4a531a5a9edad22e2275fe9c5f768
Debjit Ghosh <debjitvk18@gmail.com>
2025-10-15 13:42:22 +0530

    env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://my-backend-v4yb.onrender.com";
+const BASE_URL = process.env.REACT_APP_API_URL + "/api";
+
 
 
 /* ----------------------------- TOKEN HELPERS ----------------------------- */
